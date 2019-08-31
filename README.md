1ae201c9642b81fe0eabaeeee936189b46be0f4e
samson-ej <samson.ej@gmail.com>
2019-08-31 13:13:39 -0400

    cleaned up python file, updated README

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# suggest-din
-What's for dinner? I have a Suggest-Din!
+# Suggest-Din
+*"What's for dinner?" Well, I have a Suggest-Din!*
+
+This project aims to suggest what protein, carb, and vegetable
+you should eat based on (at first) 2 weeks worth of dinner-data.
+
+The dummy data csv, contains 2 weeks of dinners where I've encoded 
+the types of protein, carb, and vegetable. My classifications of how
+I typed the food items is debatable. In my research, I learned a large
+number of what I thought were vegetables are actually more like carbs.
+Further refinement of this in the future.
+
+## Getting Started
+
+### Prerequisites
+- Python 3.6.8
+- pandas 0.24.2
+- numpy 1.16.4
+
+### Installing
+
+## Built With
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+* Thank you **Billie Thompson** et al for creating a very nice markdown template [PurpleBooth](https://github.com/PurpleBooth)
+* * template gist - https://gist.github.com/PurpleBooth/109311bb0361f32d87a2#project-title
+
+## To-do -- move this?
+- [ ] Fill out markdown
+- [ ] Create/push new csv with better data
--- suggest-din.py
+++ suggest-din.py
@@ -1,25 +1,15 @@
 # imports
-import numpy as np
 import pandas as pd
-import csv
-import json
-import math
 from collections import Counter, OrderedDict
 
-# print("ZA WORULDO!")
-
-# populate dataframe
-
-# csv parsing, data starts as strings
-with open('/home/esamson/Desktop/projects/what4dinner/dummy.csv') as meals:
+# populate dataframe parsing csv; data starts as strings
+with open('~/suggest-din/dummy.csv') as meals:
     # fullCSV = csv.reader(meals, delimiter=',')
     mealdata = pd.read_csv(meals, delimiter=',', header=0, skiprows=0)
     df = pd.DataFrame(mealdata)
-    # print(df)
-
-    # add column for meal-name?
-
-# need dictionary to define each meat,carb,veg 
+     
+# from here on code abbreviates protein -> meat, etc.
+# lists of meats, carbs, and veggies 
 meat_list = ['chicken', 'steak','ground-beef', 'pork', 'roast-beef', 'salmon', 'white-fish',
             'tofu']
 carb_list = ['rice', 'pasta', 'spaghetti', 'macncheese', 'potato', 'risotto', 'lasagna', 'dumplings',
@@ -27,21 +17,25 @@ carb_list = ['rice', 'pasta', 'spaghetti', 'macncheese', 'potato', 'risotto', 'l
             'eggplant']
 vege_list = ['cabbage', 'brocolli', 'asparagus', 'spinach', 'brussels-sprouts', 'collards', 'celery',
             'bell-peppers', 'okra']             
-meat_dict, carb_dict, vege_dict = {},{},{}
-# carb_dict = {}
-# vege_dict = {}
-rank_meat, rank_carb, rank_vege = [],[],[]
-# rank_carb = []
-# rank_vege = []
 
+# initialize dictionaries for each 
+meat_dict, carb_dict, vege_dict = {},{},{}
+
+# initialize rank arrays
+rank_meat, rank_carb, rank_vege = [],[],[]
+
+# parse DataFrame into processing arrays
 for row in df:
     tempM = df['meat']
     tempC = df['carb']
     tempV = df['vege']
 
+# use Counter to tally counts of each
 m_count = Counter(tempM)
 c_count = Counter(tempC)
 v_count = Counter(tempV)
+
+# populate dictionaries with collected counts
 for each in tempM:
     meat_dict[meat_list[each]] = m_count[each]
 for each in tempC:
@@ -49,14 +43,12 @@ for each in tempC:
 for each in tempV:
     vege_dict[vege_list[each]] = v_count[each]
 
-# print(tempC)
-
-# prints out ascending rank of meat dictionary
+# create ordered dictionaries of each using OrderedDict sorted() function 
 ord_meat = OrderedDict(sorted(meat_dict.items(), key=lambda meat: meat[1]))
 ord_carb = OrderedDict(sorted(carb_dict.items(), key=lambda carb: carb[1]))
 ord_vege = OrderedDict(sorted(vege_dict.items(), key=lambda vege: vege[1]))
-# print(ord_vege)
 
+# populate ranked list of each
 for key in ord_meat:
     rank_meat.append(key)
 for key in ord_carb:
@@ -64,14 +56,9 @@ for key in ord_carb:
 for key in ord_vege:
     rank_vege.append(key)
 
+# analysis - TBD
 
-# print(rank_carb[2])
-# print(rank_vege)
-
-# analysis
-
-
-# return suggestion based on rank
+# returns top 3 suggestions for each based on rank
 print('For your protein, you should have ' + rank_meat[0] + ', ' + rank_meat[1] + ', or ' + rank_meat[2])
 print('For your carb, you should have ' + rank_carb[0] + ', ' + rank_carb[1] + ', or ' + rank_carb[2])
 print('For your vegetable, you should have ' + rank_vege[0] + ', ' + rank_vege[1] + ', or ' + rank_vege[2])
